87591b09f62f38a274c0900b2d137fc034779aa9
yukiohori <yukiohori@gmail.com>
2022-04-18 11:37:00 +0900

    fix: error boundary improved

--- src/components/organisms/ErrorFallback/index.tsx
+++ src/components/organisms/ErrorFallback/index.tsx
@@ -2,12 +2,18 @@ import { FallbackProps } from 'react-error-boundary';
 
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <div className="w=full flex h-screen flex-col items-center justify-center space-y-4">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button className="btn" onClick={resetErrorBoundary}>
-        Try again
-      </button>
+    <div className="w=full flex h-screen items-center justify-center">
+      <div className="mockup-code">
+        <div className="flex flex-col items-center justify-center space-y-6">
+          <p className="text-bold w-full text-center text-2xl text-error">
+            Something went wrong:
+          </p>
+          <pre>{error.message}</pre>
+          <button className="btn btn-info" onClick={resetErrorBoundary}>
+            Try again
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
